09211ea465965203f9f49905dcd05f504a70fe10
m4theus4ndr4de <matheus.andrade.lp@gmail.com>
2022-11-20 20:02:31 -0300

    handler model path

--- webapp/handler.py
+++ webapp/handler.py
@@ -5,7 +5,7 @@ from flask import Flask, request, Response
 from rossmann.Rossmann import Rossmann
 
 # load model
-model = pickle.load(open('model/model_rossmann.pkl', 'rb'))
+model = pickle.load(open('webapp/model/model_rossmann.pkl', 'rb'))
 
 # initialize API
 app = Flask(__name__)
