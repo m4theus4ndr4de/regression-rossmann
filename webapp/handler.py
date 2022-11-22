57cc0235c323ba8dcfc1d3affdd571a90a176547
m4theus4ndr4de <matheus.andrade.lp@gmail.com>
2022-11-22 13:20:41 -0300

    change numpy version

--- webapp/handler.py
+++ webapp/handler.py
@@ -5,7 +5,7 @@ from flask import Flask, request, Response
 from rossmann.Rossmann import Rossmann
 
 # load model
-model = pickle.load(open('webapp/model/model_rossmann.pkl', 'rb'))
+model = pickle.load(open('model/model_rossmann.pkl', 'rb'))
 
 # initialize API
 app = Flask(__name__)
@@ -43,5 +43,5 @@ def rossmann_predict():
         return Response('{}', status=200, mimetype='application/json')
 
 if __name__ == '__main__':
-    port = os.environ.get('PORT', 5001)
+    port = os.environ.get('PORT', 5000)
     app.run(host='0.0.0.0', port=port)
